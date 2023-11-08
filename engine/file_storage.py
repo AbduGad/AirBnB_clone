f5f411422c6501f5f7df6b6900fc2409f571af38
AbduGad <abdo79gad@gmail.com>
2023-11-08 14:57:10 +0200

    Base model class

---
+++ engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class that serializes instances to a JSON file and
+deserializes JSON file to instances
+"""
+import json
+
+
+class FileStorage():
+	"""
+	Class for saving and loading data from files.
+	"""
+	
---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" SuperClass module.
+    Define common attributes and methods for AirBnB console
+    Classes:
+        BaseModel: set common attributes & methods
+"""
+from uuid import uuid4
+import datetime
+
+
+class BaseModel():
+	"""
+	defines all common attributes/methods for other classes:
+	"""
+	def __init__(self, *args, **kwargs):
+		if len(kwargs) == 0:
+			self.id = str(uuid4())
+			self.created_at = datetime.datetime.now()
+			self.updated_at = datetime.datetime.now()
+		elif len(kwargs) > 0:
+			for key, value in kwargs.items():
+				if key == '__class__':
+					continue
+				elif key == 'created_at' or key == 'updated_at':
+					setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+					
+
+	def __str__(self):
+		print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+	def save(self):
+		self.updated_at = datetime.datetime.now()
+
+	def to_dict(self):
+		new_dict = self.__dict__.copy()
+		new_dict["__class__"] = self.__class__.__name__
+		new_dict["created_at"] = self.created_at.isoformat()
+		new_dict["updated_at"] = self.updated_at.isoformat()
+		return new_dict
+
+
