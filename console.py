5a5b47a230f43acd720d3a70adf2a2f1d118c890
Skyino7 <christambassador12@gmail.com>
2023-11-08 10:50:18 +0000

    console

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
-Test
+#!/usr/bin/python3
+
+import cmd
+# from models import storage
+# from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        print()
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
