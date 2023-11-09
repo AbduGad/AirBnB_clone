887fa2ee40b5551bddc75a23b7efc90db242f979
AbduGad <abdo79gad@gmail.com>
2023-11-09 14:24:47 +0200

    edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage():
 	"""
 	Class for saving and loading data from files.
 	"""
-	__file_path = 'Storage.json'
+	__file_path = 'file.json'
 	__objects = {}
 	
 	@property
@@ -25,3 +25,7 @@ class FileStorage():
 			self.__objects[key] = obj
 
 	def save(self):
+		with open(self.__file_path, 'w') as f:
+			
+
+
