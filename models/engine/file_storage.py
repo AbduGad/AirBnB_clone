f5a4defea9a749230cc45ce8d9114031ef2ac621
AbduGad <abdo79gad@gmail.com>
2023-11-09 14:06:31 +0200

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,10 +6,22 @@ deserializes JSON file to instances
 import json
 
 
+
 class FileStorage():
 	"""
 	Class for saving and loading data from files.
 	"""
-	__file_path = 'None'
+	__file_path = 'Storage.json'
 	__objects = {}
 	
+	@property
+	def all(self):
+		return self.__objects
+	
+	@object.setter
+	def new(self, obj):
+		if obj:
+			key = "{}.{}".format(obj.__class__.__name__, obj.id)
+			self.__objects[key] = obj
+
+	def save(self):
