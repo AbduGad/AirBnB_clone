142579d51846ded31d07d2ba8ef0840bd894314b
AbduGad <abdo79gad@gmail.com>
2023-11-11 15:53:01 +0200

    updated master

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+class that serializes instances to a JSON file and
+deserializes JSON file to instances
+"""
+import json
+from models.base_model import BaseModel
+
+
+
+class FileStorage():
+	"""
+	Class for saving and loading data from files.
+	"""
+	__file_path = 'file.json'
+	__objects = {}
+	
+	@property
+	def all(self):
+		return self.__objects
+	
+	@object.setter
+	def new(self, obj):
+		if obj:
+			key = "{}.{}".format(obj.__class__.__name__, obj.id)
+			self.__objects[key] = obj
+
+	def save(self):
+		for key, value in self.__objects.items():
+			self.__objects[key] = value.to_dict()
+		with open(self.__file_path, 'w') as f:
+			json.dump(self.__objects, f)
+
+	def reload(self):
+		data = {}
+		try:
+			with open(self.__file_path, 'r') as f:
+				data = json.load(f)
+			for key, value in data.items():
+				obj = eval(value['__class__'])(**value)
+				self.__objects[key] = obj
+		except FileNotFoundError:
+			pass
