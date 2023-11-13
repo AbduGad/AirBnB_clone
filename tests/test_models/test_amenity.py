0020e91ea23a80ee3d5b263fc28b3bcb7cc97f75
AbduGad <abdo79gad@gmail.com>
2023-11-14 00:14:39 +0200

    aminety test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,6 +12,7 @@ from models import storage
 from models.base_model import BaseModel
 from models.amenity import Amenity
 import os
+import pycodestyle
 
 
 class test_amenity(unittest.TestCase):
@@ -28,3 +29,32 @@ class test_amenity(unittest.TestCase):
 			os.remove(storage.__file_path)
 		except Exception:
 			pass
+
+	def test_pep8_Amen(self):
+		"""Tests pep8"""
+		style = pycodestyle.StyleGuide(quiet=True)
+		pycode = style.check_files(['models/amenity.py'])
+		self.assertEqual(pycode.total_errors, 0, "fix your pycodestyle")
+
+	def test_docstring_Amen(self):
+		"""test for docstrings"""
+		self.assertIsNotNone(self.amenity1.__doc__)
+
+	def test_attribute(self):
+		"""chekcing if amenity have attibutes"""
+		self.assertTrue('id' in self.amenity1.__dict__)
+		self.assertTrue('created_at' in self.amenity1.__dict__)
+		self.assertTrue('updated_at' in self.amenity1.__dict__)
+		self.assertTrue('name' in self.amenity1.__dict__)
+
+	def test_inheritance(self):
+		"""test if inheritance works"""
+		self.assertTrue(issubclass(self.amenity1.__class__, BaseModel), True)
+
+	def test_attrtype(self):
+		"""test attr types in instance"""
+		self.assertEqual(type(self.amenity1.name), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
